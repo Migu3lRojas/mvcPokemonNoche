4f757896a8feeab538ab05169baf8ebe418fd781
Miguel Rojas <juan.mr952@gmail.com>
2023-05-10 19:12:11 -0500

    Primer commit MVC

---
+++ view/roles.frm.php
@@ -0,0 +1,16 @@
+<?php   include_once "header.php"; ?>
+<div class="row">
+    <h1></h1>Formulario Roles</div>
+
+    <form class="form-floating">
+  <input type="email" class="form-control is-invalid" id="floatingInputInvalid" placeholder="name@example.com" value="test@example.com">
+  <label for="floatingInputInvalid">Invalid input</label>
+</form>
+
+
+
+<div class="row bg-dark"></div>
+<div class="text-while text-center">tabla de roles</div>
+
+
+<?php include_once "footer.php"; ?>
